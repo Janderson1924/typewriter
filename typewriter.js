25d329ab58cf700df453c5b371d78e8aa93139e1
Justin <justinanderson101@gmail.com>
2021-03-09 20:59:16 +0000

    typewriter function

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0; // start delay at 0 seconds
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char); // print characters in 'char' loop
+  }, delay)
+  delay += 50; // increase delay by 50 each time
+};
+
+setTimeout(() => {
+  console.log(' ');
+}, delay+1); // once loop is finished, this will console log a space after the sentence to avoid merginf with temrinal command line
